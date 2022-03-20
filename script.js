8f105a1050b43146d93381ba8593ea2e817aabb0
Lindineis <95833998+neismerce@users.noreply.github.com>
2022-03-20 20:23:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var listaFilmes = [
+  "https://i.imgur.com/Q8P0tIS.jpg",
+  "https://media.fstatic.com/k5oDkgDNCMbIs-Y4mLzH1tS-1Vo=/290x478/smart/media/movies/covers/2011/07/f004bc4434bd1aecfbaf45a603293862.jpg",
+  "https://br.web.img3.acsta.net/pictures/14/12/02/22/15/327211.jpg"
+];
+for (var indice = 0; indice < listaFilmes.length; indice++) {
+  document.write("<img src=" + listaFilmes[indice] + ">");
+}
